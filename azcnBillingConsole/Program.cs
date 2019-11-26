4a90c3cf39376bb7cba3048585542346ae77e152
Harvey <tigerush80@hotmail.com>
2019-11-26 15:51:27 +0800

    test

--- azcnBillingConsole/Program.cs
+++ azcnBillingConsole/Program.cs
@@ -6,7 +6,8 @@ namespace azcnBillingConsole
     internal class Program
     {
         private static readonly string enrollmentNumber = "V5701611S0381";
-        private static readonly string accessKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6InJtSUhHNjNQZElOcWxQenFnQWd1eXZISzdMbyJ9.eyJFbnJvbGxtZW50TnVtYmVyIjoiVjU3MDE2MTFTMDM4MSIsIklkIjoiYWJiYjU5OGUtNjIwNy00N2ZkLWJiNDgtNGQ2YjFmZDRiNWY3IiwiUmVwb3J0VmlldyI6IkVudGVycHJpc2UiLCJQYXJ0bmVySWQiOiIiLCJEZXBhcnRtZW50SWQiOiIiLCJBY2NvdW50SWQiOiIiLCJpc3MiOiJlYS5taWNyb3NvZnRhenVyZS5jb20iLCJhdWQiOiJjbGllbnQuZWEubWljcm9zb2Z0YXp1cmUuY29tIiwiZXhwIjoxNTgwMzY0ODc4LCJuYmYiOjE1NjQ0NjcyNzh9.lwdnXPTF-aadTp0-H3L8M7KTCGsJoBNkY0if1o3K2VdSw7QJiM7cCjSh57juueadI22l0q0RnZTGRvgXfEv2o4CqP0bKt2esyq6-UbKL-y51nlRQSetvlfU3NXwuK6n3oi6wfTaqDAdf1u2Y1ozaQDnJc1lUhospBIpChWm0ocZwS_qjy9KiEzG5xc4EMHzYgnk1noRSk_7gUO-3Ed3VyPf5nJp29UdxV33u0rde6O7zdyIf6ZMaMiieCiAGHoqsc7M_h0-ZSk0wLBjhpXih_ntTbCaAIAnUIFhaWyUYJR6zFzreFHIS-TE-Tsk7bwfFwKJq15rCmZ2aTEnM37nGVA";
+        private static readonly string accessKey = "{replace the access key of yours here}";
+        // private static readonly string accessKey = "eyJ0eXAiOiJKV1QiLCJhbG.........................";
 
         private static void Main(string[] args)
         {
